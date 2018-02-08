1f3bf34038a12d426b6f4552b1d638902abed9c7
Christian Puhrsch <cpuhrsch@fb.com>
2018-02-08 16:35:56 -0700

    more

--- baselines/avx_sum_csv.py
+++ baselines/avx_sum_csv.py
@@ -5,27 +5,32 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('file')
+    parser.add_argument('file', nargs='+')
     args = parser.parse_args()
-    values = {}
 
-    with open(args.file) as f:
-        for line in f:
-            line = line.split(",")
-            for cell in line:
-                k, v =map(lambda x: x.strip(), cell.split(":"))
-                k = k[1:]
-                v = v[:-1]
-                if k not in values:
-                    values[k] = []
-                values[k].append(v)
-    print(",".join(values.keys()))
-    keys = values.keys()
-    for i in range(len(values[keys[0]])):
-        line = []
-        for k in keys:
-            line.append(values[k][i])
-        print(",".join(line))
+    keys = None
+    for file1 in args.file:
+        values = {}
+        with open(file1) as f:
+            for line in f:
+                line = line.split(",")
+                for cell in line:
+                    k, v =map(lambda x: x.strip(), cell.split(":"))
+                    k = k[1:]
+                    v = v[:-1]
+                    if k not in values:
+                        values[k] = []
+                    values[k].append(v)
+        if keys is None:
+            keys = values.keys()
+            print(",".join(values.keys()))
+        else:
+            assert(keys == values.keys())
+        for i in range(len(values[keys[0]])):
+            line = []
+            for k in keys:
+                line.append(values[k][i])
+            print(",".join(line))
 
 
 
--- baselines/avx_sum_grid.py
+++ baselines/avx_sum_grid.py
@@ -2,8 +2,6 @@
 
 import argparse
 
-CMD = "./avx_sum.exe"
-
 GRID = [
     (64, 64),
     (128, 128),(256, 256),(128, 512),(512, 128),(1024, 1024),(2048, 2048),(4096, 4096),(512, 4096),(4096, 512)
@@ -12,13 +10,14 @@ GRID = [
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('cmd')
     parser.add_argument('run')
     parser.add_argument('repeat', type=int) # Repeat to increase benchmark precision
     
     args = parser.parse_args()
     grid = []
     for (size1, size2) in GRID:
-        grid.append(CMD + " -size1 " + str(size1) + " -size2 " + str(size2) + " -epoch -1 " + args.run)
+        grid.append(args.cmd + " -size1 " + str(size1) + " -size2 " + str(size2) + " -epoch -1 " + args.run)
     for g in grid:
         for _ in range(args.repeat):
             print(g)
