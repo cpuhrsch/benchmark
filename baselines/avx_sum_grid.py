c44386ea32f148c5d075a711a7a54e8f4c7235a9
Christian Puhrsch <cpuhrsch@fb.com>
2018-02-12 15:00:26 -0700

    tbb numa

--- baselines/avx_sum_grid.py
+++ baselines/avx_sum_grid.py
@@ -8,10 +8,11 @@ GRID_sizes = [
     (128, 128),(256, 256),(128, 512),(512, 128),(1024, 1024),(2048, 2048),(4096, 4096),(512, 4096),(4096, 512)
 ]
 
-GRID_thresholds = [4096, 16384, 65536, 1048576, 4194304]
+GRID_thresholds = [65536]
+GRID_numa = ["echo -n \"(numa:\t1),\"; taskset -c 0-19,40-59 ", "echo -n \"(numa:\t0),\";"]
 
 # GRID_functions = ["-run_sum_tbb sum_impl_tbb_2", "-run_sum_tbb sum_impl_tbb", "-run_sum sum_impl21"]
-GRID_functions = ["-run_sum_tbb sum_impl_tbb_4"]
+GRID_functions = ["-run_sum_tbb sum_impl_tbb"]
 
 
 if __name__ == "__main__":
@@ -21,8 +22,8 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
     grid = []
-    for ((size1, size2), threshold, function) in list(itertools.product(GRID_sizes, GRID_thresholds, GRID_functions)):
-        grid.append(args.cmd + " -size1 " + str(size1) + " -size2 " + str(size2) + " -epoch -1 " + function + " -threshold " + str(threshold))
+    for (numa, (size1, size2), threshold, function) in list(itertools.product(GRID_numa, GRID_sizes, GRID_thresholds, GRID_functions)):
+        grid.append(numa + args.cmd + " -size1 " + str(size1) + " -size2 " + str(size2) + " -epoch -1 " + function + " -threshold " + str(threshold))
     for g in grid:
         for _ in range(args.repeat):
             print(g)
