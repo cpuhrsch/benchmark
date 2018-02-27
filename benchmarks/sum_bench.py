36f2c827355c711098015d68b0fc7297e86133fb
Christian Puhrsch <cpuhrsch@fb.com>
2018-02-27 16:32:36 -0700

    better sum bench

--- benchmarks/sum_bench.py
+++ benchmarks/sum_bench.py
@@ -1,37 +1,82 @@
 import numpy as np
 import torch
 import gc
+import random
 import time
+import math
+from torch.autograd import Variable
+
+
+#TODO: Compare to CUDA
+
+def generate_sizes(size, dims):
+    sizes = []
+    for d in range(dims):
+        if d < dims - 1:
+            rs = random.randint(int(math.pow(size / dims, 1.0 / dims)), int(math.pow(size, 1.2 / dims)))
+            if rs >0:
+                size = size / rs
+        else:
+            rs = size
+        if rs <= 0:
+            rs = 1
+        sizes.append(rs)
+
+    return sizes
+
+def run_instance(sizes, dims, runs, test, numpy):
+
+    # sizes = [5, 100]
+
+    v = np.random.rand(*sizes).astype(np.float32)
+    print(v.dtype)
+    tv = Variable(torch.from_numpy(v).contiguous())
+    gc.collect()
+    start = time.time()
+    for d in range(dims - 1):
+        for _ in range(runs):
+            if numpy or test:
+                vr = v.sum(d)
+            if not numpy or test:
+                tvr = tv.sum(d)
+            if test:
+                vr = Variable(torch.from_numpy(vr))
+                if not torch.equal(tvr, vr):
+                    print("ERROR!")
+                    print(((tvr - vr).abs() / vr).mean())
+                    print("(tvr - vr).abs() / vr")
+                    print((tvr - vr).abs() / vr)
+                    print("tvr")
+                    print(tvr)
+                    print("vr")
+                    print(vr)
+                    print("ERROR!")
+                    import sys
+                    sys.exit(1)
+    t_time = time.time() - start
+    return t_time
+
+
 
 if __name__ == "__main__":
-    sample_size = 10000;
-    size = 1000000;
-    enable_numpy = False
-    enable_torch = True
+    sample_size = 1
+#    sample_size = 1
+    to_process = 1000 * 1000 * 1000
+    enable_numpy = True
+    enable_test = False
     # enable_numpy = True
     # enable_torch = False
-    v = np.random.rand(size)
-    tv = torch.from_numpy(v).contiguous()
-    status = ""
-    nps = 0
-    ts = 0
-    if enable_numpy:
-        gc.collect()
-        start = time.time()
-        for _ in range(sample_size):
-            s = v.sum()
-            # s /= size
-            # v *= s
-        nps = s
-        status += "numpy: {} nps: {}".format(time.time() - start, nps)
-    if enable_torch:
-        gc.collect()
-        start = time.time()
-        for _ in range(sample_size):
-            s = tv.sum()
-            # s /= size
-            # tv *= s
-        ts = s
-        t_time = time.time() - start
-        status += "torch: {} ts: {}".format(time.time() - start, ts)
-    print(status)
+    ttime = 0.0
+    all_settings = []
+    random.seed(1234)
+    for runs_ in range(2, 6):
+        runs = math.pow(10, runs_)
+        size = int(to_process / runs)
+        runs = int(runs)
+        for dims in range(1, 5):
+            all_settings.append((runs, generate_sizes(size, dims), dims))
+    for setting in all_settings:
+        runs, sizes, dims = setting
+        print("runs: {} sizes: {} dims: {}".format(str(runs), str(sizes), str(dims)))
+        ttime += run_instance(sizes, dims, runs, enable_test, enable_numpy)
+    print("ttime: " + str(ttime) + "s")
