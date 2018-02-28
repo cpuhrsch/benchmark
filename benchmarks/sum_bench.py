50f93b2c0b441d5a44e2f75b35985c1ba864894c
Christian Puhrsch <cpuhrsch@fb.com>
2018-02-28 13:40:55 -0700

    better comparison etc

--- benchmarks/sum_bench.py
+++ benchmarks/sum_bench.py
@@ -5,9 +5,12 @@ import random
 import time
 import math
 from torch.autograd import Variable
+import argparse
 
 
 #TODO: Compare to CUDA
+#Compare with master
+#Double check dispatch etc.
 
 def generate_sizes(size, dims):
     sizes = []
@@ -24,59 +27,122 @@ def generate_sizes(size, dims):
 
     return sizes
 
-def run_instance(sizes, dims, runs, test, numpy):
+def run_instance(sizes, dims, runs, test, numpy, dattype, d):
 
     # sizes = [5, 100]
 
-    v = np.random.rand(*sizes).astype(np.float32)
-    print(v.dtype)
+    v = np.random.rand(*sizes).astype(dattype)
     tv = Variable(torch.from_numpy(v).contiguous())
     gc.collect()
     start = time.time()
-    for d in range(dims - 1):
-        for _ in range(runs):
-            if numpy or test:
+    error = None
+    for _ in range(runs):
+        if numpy or test:
+            if d is None:
+                vr = v.sum()
+            else:
                 vr = v.sum(d)
-            if not numpy or test:
+        if not numpy or test:
+            if d is None:
+                tvr = tv.sum()
+            else:
                 tvr = tv.sum(d)
-            if test:
+        if test:
+            if d is not None:
                 vr = Variable(torch.from_numpy(vr))
-                if not torch.equal(tvr, vr):
-                    print("ERROR!")
-                    print(((tvr - vr).abs() / vr).mean())
-                    print("(tvr - vr).abs() / vr")
-                    print((tvr - vr).abs() / vr)
-                    print("tvr")
-                    print(tvr)
-                    print("vr")
-                    print(vr)
-                    print("ERROR!")
-                    import sys
-                    sys.exit(1)
+                if vr.nonzero().sum() != 0:
+                    error = ((tvr - vr).abs() / vr).mean()
+                    error = error.item()
+                else:
+                    error = 0
+            else:
+                error = abs(tvr.item() - vr.item())
+            if error > 0:
+                assert(runs == 1)
+            else:
+                error = None
+
+                #print("(tvr - vr).abs() / vr")
+                #print((tvr - vr).abs() / vr)
+                #print("tvr")
+                #print(tvr)
+                #print("vr")
+                #print(vr)
+                # import sys
+                # sys.exit(1)
     t_time = time.time() - start
-    return t_time
+    return t_time, error
 
-
+def run_settings(enable_numpy, all_settings, dattype):
+    use_numpy = [False]
+    if enable_numpy:
+        use_numpy += [True]
+    header = ["dtype"]
+    first_setting = True
+    all_speedup = 0
+    for setting in all_settings:
+        runs, sizes, dims, d = setting
+        status = ""
+        status += str(dattype) + ": "
+        if first_setting:
+            header[-1] += " " * (len(str(dattype)) - len(header[-1]) + 1)
+        for en in use_numpy:
+            ltime, error = run_instance(sizes, dims, runs, enable_test, en, dattype, d)
+            if en:
+                if first_setting:
+                    header += ["library"]
+                status += "numpy:  "
+            else:
+                if first_setting:
+                    header += ["library"]
+                status += "torch:  "
+            header += [" "*6 + "runs", " sizes" + " " * 15, " "*8 + "time", " " * 4 + "dim"]
+            d_str = str(d)
+            if d is None:
+                d_str = "all"
+            status += "{:10}  {:20} {:10.2f}ms {:8}".format(runs, str(sizes), ltime * 1000, d_str)
+            if en:
+                ttime[1] += ltime
+                speedup = ttime[1] / ttime[0]
+                status += "speedup: " + str(speedup)
+                all_speedup += speedup
+            else:
+                ttime[0] += ltime
+            if error is not None:
+                status += " error!: " + str(error)
+        if first_setting:
+            print(" ".join(header))
+        first_setting = False
+        print(status)
+    print("Average speedup: " + str(all_speedup / len(all_settings)))
+    print("-" * len(status))
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--enable_numpy', action='store_true')
+    parser.add_argument('--test', action='store_true')
+    parser.add_argument('mb', type=int, help='MB to process')
+    
+    args = parser.parse_args()
+
     sample_size = 1
-#    sample_size = 1
-    to_process = 1000 * 1000 * 1000
-    enable_numpy = True
-    enable_test = False
-    # enable_numpy = True
-    # enable_torch = False
-    ttime = 0.0
+    to_process = 1000 * 1000 * args.mb
+    print("Processing " + str(to_process / 1e6) + "MB")
+    enable_numpy = args.enable_numpy
+    enable_test = args.test
+    if enable_test:
+        print("Testing!")
+    ttime = [0.0, 0.0]
     all_settings = []
     random.seed(1234)
     for runs_ in range(2, 6):
         runs = math.pow(10, runs_)
         size = int(to_process / runs)
         runs = int(runs)
+        if enable_test:
+            runs = 1
         for dims in range(1, 5):
-            all_settings.append((runs, generate_sizes(size, dims), dims))
-    for setting in all_settings:
-        runs, sizes, dims = setting
-        print("runs: {} sizes: {} dims: {}".format(str(runs), str(sizes), str(dims)))
-        ttime += run_instance(sizes, dims, runs, enable_test, enable_numpy)
-    print("ttime: " + str(ttime) + "s")
+            for d in ([None] + range(dims - 1)):
+                all_settings.append((runs, generate_sizes(size, dims), dims, d))
+    for dattype in [np.float64, np.float32, np.int64, np.int32]:
+        run_settings(enable_numpy, all_settings, dattype)
